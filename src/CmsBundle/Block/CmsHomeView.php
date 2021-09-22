4f725610e8e2ec04aaced9eb3f7abeece07c7a63
Matheus Gontijo <matheus@matheusgontijo.com>
2021-09-22 20:52:45 -0300

    updated release date

--- src/CmsBundle/Block/CmsHomeView.php
+++ src/CmsBundle/Block/CmsHomeView.php
@@ -13,13 +13,13 @@ class CmsHomeView extends CmsView
             [
                 'title'    => '001 - Your contribution is important to the Magento Community',
                 'link'     => $this->getUrl('episode/001-your-contribution-is-important-to-the-magento-community'),
-                'date'     => '09/24/2021',
+                'date'     => '09/23/2021',
                 'duration' => '9:18s',
             ],
             [
                 'title'    => '000 - Introduction',
                 'link'     => $this->getUrl('episode/000-introduction'),
-                'date'     => '09/24/2021',
+                'date'     => '09/23/2021',
                 'duration' => '2:07s',
             ],
         ];
